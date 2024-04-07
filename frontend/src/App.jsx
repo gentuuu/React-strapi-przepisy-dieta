197464d29cdf12462776e709da3f6e303cdf7f1a
gentuuu <60502683+gentuuu@users.noreply.github.com>
2024-04-07 19:22:23 +0200

    Install apollo client graphql frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,15 @@ import Recipe from './components/Recipe/Recipes'
 import Diets from './components/Diet/Diets'
 import Blogs from './components/Blogs/Blogs'
 import RecipeItem from './components/Recipe/RecipeItem'
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
+
+
+// initialize apollo client
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache()
+})
+
 
 function App() {
 
@@ -14,15 +23,17 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/przepisy' element={<Recipe />} />
-          <Route path='/przepisy/post' element={<RecipeItem />} />
-          <Route path='/diety' element={<Diets />} />
-          <Route path='/blogs' element={<Blogs />} />
-        </Routes>
-        <Footer />
+        <ApolloProvider client={client}>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/przepisy' element={<Recipe />} />
+            <Route path='/przepisy/post' element={<RecipeItem />} />
+            <Route path='/diety' element={<Diets />} />
+            <Route path='/blogs' element={<Blogs />} />
+          </Routes>
+          <Footer />
+        </ApolloProvider>
       </BrowserRouter>
     </>
   )
